fcdc1517ceded31847edd9589e62ef5d768c24b8
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-14 23:03:57 +0300

    Change color of blocks on click

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector('.container');
+const squares = document.createElement('div');
+squares.classList.add('squares');
 
 const gridSize = 16;
 for (let i = 0; i < gridSize; i++) {
   for (let j = 0; j < gridSize; j++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    container.appendChild(square);
+    square.setAttribute('id', `itm-${i + 1}-by-${j + 1}`)
+    squares.appendChild(square);
   }
 }
+
+container.appendChild(squares);
+
+Array.from(squares.childNodes).forEach(element => {
+  element.addEventListener('click', (e) => e.target.style = 'background-color: red;');
+});

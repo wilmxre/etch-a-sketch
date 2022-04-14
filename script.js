37940db0bc15fcc0a533e1affe61b6488a7db353
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-15 00:00:36 +0300

    Refactor code and create the starting point of resize

--- script.js
+++ script.js
@@ -2,25 +2,37 @@ const container = document.querySelector('.container');
 const squares = document.createElement('div');
 squares.classList.add('squares');
 
-const gridSize = 16;
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.setAttribute('id', `itm-${i + 1}-by-${j + 1}`)
-    squares.appendChild(square);
+let gridSize = 16;
+
+let generateSquares = (size) => {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.setAttribute('id', `itm-${i + 1}-by-${j + 1}`)
+      squares.appendChild(square);
+    }
   }
+
+  return squares;
 }
 
-container.appendChild(squares);
+container.appendChild(generateSquares(gridSize));
 
 squares.childNodes.forEach(element => {
   element.addEventListener('mouseover', (e) => e.target.style = 'background-color: red;');
 });
 
 const clearBtn = document.querySelector('#clear');
-clearBtn.addEventListener('click', () => {
+let clearAndResize = () => {
+  console.log(1);
   squares.childNodes.forEach(element => {
     element.style = 'background-color: white;';
-  })
-})
+  });
+
+  gridSize = parseInt(prompt('Enter the size of your new canvas!'));
+  squares.textContent = '';
+  generateSquares(gridSize);
+}
+
+clearBtn.addEventListener('click', clearAndResize);

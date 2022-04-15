be0a15065eb773baba8b21bfff8fec537e368e39
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-15 16:59:16 +0300

    Remove row/col sectioning
    Couldn't access the grandchildren of the squares node, so i removed it,
    and it turned out it was unnecessary either way.

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 const container = document.querySelector('.container');
 const squares = document.createElement('div');
 squares.classList.add('squares');
+const clearBtn = document.querySelector('#clear');
+const resizeBtn = document.querySelector('#resize');
 
 let gridSize = 16;
 
 let generateSquares = (size) => {
   for (let i = 0; i < size; i++) {
-    const squareRow = document.createElement('div');
-    squareRow.classList.add('square-row', `square-row-${i + 1}`);
     for (let j = 0; j < size; j++) {
       const square = document.createElement('div');
       square.classList.add('square');
       square.setAttribute('id', `itm-${i + 1}-by-${j + 1}`)
-      squareRow.appendChild(square);
+      squares.append(square);
     }
-    squares.append(squareRow);
   }
-
 
   return squares;
 }
 
-container.appendChild(generateSquares(gridSize));
+let fillWithColor = () => {
+  squares.childNodes.forEach(element => {
+    element.addEventListener('mouseover', (e) => e.target.style = 'background-color: red;');
+  });
+}
 
-squares.childNodes.forEach(element => {
-  element.addEventListener('mouseover', (e) => e.target.style = 'background-color: red;');
-});
-
-const clearBtn = document.querySelector('#clear');
-let clearAndResize = () => {
+let clearSketchpad = () => {
   squares.childNodes.forEach(element => {
     element.style = 'background-color: white;';
   });
+}
 
+let resizeSketchpad = () => {
   gridSize = parseInt(prompt('Enter the size of your new canvas!'));
   squares.textContent = '';
   container.style = `--size: ${gridSize}`;
   generateSquares(gridSize);
+  fillWithColor();
 }
 
-clearBtn.addEventListener('click', clearAndResize);
+window.onload = () => {
+  container.appendChild(generateSquares(gridSize));
+  fillWithColor();
+  clearBtn.addEventListener('click', clearSketchpad);
+  resizeBtn.addEventListener('click', resizeSketchpad);
+}
+
+console.log(squares.childNodes)
